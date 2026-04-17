518276b63500e4653850b17123ea64eea6a0a78e
Marek Hummel <marek.hummel3@gmail.com>
2026-04-17 20:19:07 +0200

    More flexible ARAM indicator

--- sedidata-tui/src/ui/views/game.rs
+++ sedidata-tui/src/ui/views/game.rs
@@ -104,8 +104,8 @@ fn champ_select_aram_view(ctrl: &Controller) -> TextCreationResult {
         Some(champ_select_info) => {
             let queue = ctrl.lookup.get_queue(champ_select_info.queue_id)?;
 
-            match queue.pick_mode.as_str() {
-                "AllRandomPickStrategy" => {
+            match (queue.pick_mode.as_str(), queue.gamemode.as_str()) {
+                ("AllRandomPickStrategy", _) | (_, "ARAM") | (_, "KIWI") => {
                     let (current_champ, team_champs) = get_team_selections(&champ_select_info);
 
                     lines.push(styled_line!("Currently selected champ:"; Color::Rgb(200, 150, 0)));
